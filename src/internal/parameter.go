d1d667c955b26decdc75655f9574007264445931
Lewis Edginton <lewisedge@gmail.com>
2021-05-16 19:19:49 +0100

    Added AddPlaceholders method to interface

--- src/internal/parameter.go
+++ src/internal/parameter.go
@@ -2,6 +2,7 @@ package internal
 
 type Parameter interface {
 	GetValue() string
+	AddPlaceholders([]Placeholder)
 }
 
 type SimpleParameter struct {
@@ -12,15 +13,23 @@ func (p *SimpleParameter) GetValue() string {
 	return p.Value
 }
 
+func (p *SimpleParameter) AddPlaceholders(ph []Placeholder) {
+	return
+}
+
 type ParameterWithPlaceholders struct {
 	Value        string
 	Placeholders []Placeholder
 }
 
-func (p *ParameterWithPlaceholders) GetValue() string {
-	outString := p.Value
-	for _, placeholder := range p.Placeholders {
+func (pwp *ParameterWithPlaceholders) GetValue() string {
+	outString := pwp.Value
+	for _, placeholder := range pwp.Placeholders {
 		outString = placeholder.Replace(outString)
 	}
 	return outString
+}
+func (pwp *ParameterWithPlaceholders) AddPlaceholders(ph []Placeholder) {
+	pwp.Placeholders = append(pwp.Placeholders, ph...)
+	return
 }
