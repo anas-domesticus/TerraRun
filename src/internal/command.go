f81aab1d15ecc2c697511b4496afec194a76e465
Lewis Edginton <lewisedge@gmail.com>
2021-05-16 19:20:12 +0100

    Implemented methods for command struct + tests

--- src/internal/command.go
+++ src/internal/command.go
@@ -1,8 +1,42 @@
 package internal
 
+import (
+	"bytes"
+	"os/exec"
+)
+
+type CommandIface interface {
+	Execute() error
+}
+
 type Command struct {
 	Binary     string
 	Parameters []Parameter
 }
 
-//func (c *Command) Execute(e Environment, )
+//Get config, inc env & cmd
+//Get stacks
+//build & run command against stack
+//capture output, squirt to stdout
+
+func (c *Command) Execute(cfg *Config, stack *TerraformStack) ([]byte, []byte, error) {
+	// First we populate a slice of standard placeholders for this run
+	stdPlaceholders := stack.GetStackPlaceholders()
+	stdPlaceholders = append(stdPlaceholders, cfg.Env.GetEnvPlaceholder())
+
+	// Getting the params we'll actually be using
+	var params []string
+	for i, v := range c.Parameters {
+		c.Parameters[i].AddPlaceholders(stdPlaceholders)
+		params = append(params, v.GetValue())
+	}
+
+	// Building the command
+	cmd := exec.Command(c.Binary, params...)
+	var stdOut, stdErr bytes.Buffer
+	cmd.Stdout = &stdOut
+	cmd.Stderr = &stdErr
+	cmd.Dir = stack.Path
+	err := cmd.Run()
+	return stdOut.Bytes(), stdErr.Bytes(), err
+}
---
+++ src/internal/command_test.go
@@ -0,0 +1,83 @@
+package internal
+
+import (
+	"github.com/stretchr/testify/assert"
+	"testing"
+)
+
+func TestExecute(t *testing.T) {
+
+	tests := []struct {
+		Name       string
+		Binary     string
+		Params     []Parameter
+		WantErr    bool
+		WantStdOut string
+		WantStdErr string
+	}{
+		{
+			"missing_binary",
+			"",
+			nil,
+			true,
+			"",
+			"",
+		},
+		{
+			"non_zero_exit_code",
+			"/bin/false",
+			nil,
+			true,
+			"",
+			"",
+		},
+		{
+			"success",
+			"/bin/true",
+			nil,
+			false,
+			"",
+			"",
+		},
+		{
+			"output_test",
+			"/usr/bin/echo",
+			[]Parameter{&SimpleParameter{Value: "foo"}},
+			false,
+			"foo\n",
+			"",
+		},
+		{
+			"env_param_injection",
+			"/usr/bin/echo",
+			[]Parameter{&ParameterWithPlaceholders{Value: "{{Environment}}"}},
+			false,
+			"test\n",
+			"",
+		},
+		{
+			"stack_param_injection",
+			"/usr/bin/echo",
+			[]Parameter{&ParameterWithPlaceholders{Value: "{{StackRelPath}}"}},
+			false,
+			"testdata/valid_stack\n",
+			"",
+		},
+	}
+	cfg := Config{Env: Environment{Name: "test"}}
+	stack := TerraformStack{Path: "testdata/valid_stack"}
+
+	for _, tc := range tests {
+		t.Run(tc.Name, func(t *testing.T) {
+			cmd := Command{Parameters: tc.Params, Binary: tc.Binary}
+			stdOut, stdErr, err := cmd.Execute(&cfg, &stack)
+			if tc.WantErr {
+				assert.Error(t, err)
+			} else {
+				assert.NoError(t, err)
+			}
+			assert.Equal(t, tc.WantStdOut, string(stdOut))
+			assert.Equal(t, tc.WantStdErr, string(stdErr))
+		})
+	}
+}
