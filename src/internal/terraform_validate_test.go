9208570fe19dc0018511cef12e25c0ea56b20867
Lewis Edginton <lewis.edginton@starlingbank.com>
2021-05-18 18:36:14 +0100

    Imrpoving tests

--- src/internal/terraform_validate_test.go
+++ src/internal/terraform_validate_test.go
@@ -9,14 +9,16 @@ import (
 func TestTFValidate(t *testing.T) {
 
 	tests := []struct {
-		Name    string
-		Path    string
-		WantErr bool
-		Valid   bool
+		Name          string
+		Path          string
+		WantErr       bool
+		WantOutputErr bool
+		Valid         bool
 	}{
 		{
 			"empty_string",
 			"",
+			false,
 			false,
 			true,
 		},
@@ -25,16 +27,19 @@ func TestTFValidate(t *testing.T) {
 			"somewhere_made_up",
 			true,
 			false,
+			false,
 		},
 		{
 			"non_tf_dir",
 			"./testdata/non_tf_dir",
+			false,
 			false,
 			true,
 		},
 		{
 			"valid_stack",
 			"testdata/valid_stack",
+			false,
 			false,
 			true,
 		},
@@ -43,22 +48,20 @@ func TestTFValidate(t *testing.T) {
 			"testdata/invalid_stack",
 			true,
 			false,
+			false,
 		},
 	}
 
-	initCmd := GetTerraformInit()
-	validateCmd := GetTerraformValidate()
-
 	for _, tc := range tests {
 		t.Run(tc.Name, func(t *testing.T) {
-			// init stage
-			initCmd.Parameters = append(initCmd.Parameters, &SimpleParameter{Value: "-backend=false"})
-			output := initCmd.Execute(Config{BaseDir: "./"}, TerraformStack{Path: tc.Path})
-
-			// & validate
-			valOutput := ValidateOutput{}
-			output = validateCmd.Execute(Config{BaseDir: "./"}, TerraformStack{Path: tc.Path})
+			output, err := ValidateStack(Config{BaseDir: "./"}, TerraformStack{Path: tc.Path})
 			if tc.WantErr {
+				assert.Error(t, err)
+			} else {
+				assert.NoError(t, err)
+			}
+			valOutput := ValidateOutput{}
+			if tc.WantOutputErr {
 				assert.Error(t, output.Error)
 			} else {
 				assert.NoError(t, output.Error)
