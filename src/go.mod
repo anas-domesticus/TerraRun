5cf34aa22989be3ff3dedb4accbfdb73dcb4540f
Lewis Edginton <lewisedge@gmail.com>
2021-05-20 20:28:06 +0100

    Got basic report working

--- src/go.mod
+++ src/go.mod
@@ -3,7 +3,6 @@ module gitlab.com/lewisedginton/aws_common/terraform_wrapper/src
 go 1.16
 
 require (
-	github.com/alexkappa/mustache v0.0.0-20191113130723-8bb9cfca2bfa // indirect
 	github.com/fatih/color v1.7.0
 	github.com/hashicorp/terraform-json v0.11.0
 	github.com/spf13/cobra v1.1.3
--- src/internal/terraform_html.go
+++ src/internal/terraform_html.go
@@ -20,12 +20,63 @@ func (sos *ShowOutputSet) GenerateHTMLReport() []byte {
 		return nil
 	}
 	var rendered bytes.Buffer
-	err = t.Execute(&rendered, sos)
+	err = t.Execute(&rendered, sos.buildTableData())
 	if err != nil {
 		return nil
 	}
 
 	return rendered.Bytes()
+}
+
+type HTMLTableData struct {
+	StackName     string
+	Create        int
+	Update        int
+	Noop          int
+	Destroy       int
+	ChangeDetails []ChangeDetail
+}
+
+type ChangeDetail struct {
+	ResourceName string
+	Before       string
+	After        string
+}
+
+func (sos *ShowOutputSet) buildTableData() []HTMLTableData {
+	var outSlice []HTMLTableData
+	for _, v := range *sos {
+		var createCount, updateCount, destroyCount, noopCount int
+		var changeDetails []ChangeDetail
+		for _, change := range v.Plan.ResourceChanges {
+			if change.Change.Actions.Create() {
+				createCount++
+			}
+			if change.Change.Actions.Delete() {
+				destroyCount++
+			}
+			if change.Change.Actions.Update() {
+				updateCount++
+			}
+			if change.Change.Actions.NoOp() {
+				noopCount++
+			}
+			changeDetails = append(changeDetails, ChangeDetail{
+				ResourceName: change.Name,
+				Before:       "foo",
+				After:        "bar",
+			})
+		}
+		outSlice = append(outSlice, HTMLTableData{
+			StackName:     v.Stack.Path,
+			Create:        createCount,
+			Update:        updateCount,
+			Noop:          noopCount,
+			Destroy:       destroyCount,
+			ChangeDetails: changeDetails,
+		})
+	}
+	return outSlice
 }
 
 func GetJSONString(input interface{}) string {
