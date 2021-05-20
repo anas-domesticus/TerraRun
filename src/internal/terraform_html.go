db2204b42fa20bf764d32cbccf0a7911e01a32eb
Lewis Edginton <lewis.edginton@starlingbank.com>
2021-05-20 14:47:34 +0100

    First hack at actually getting a report generating

--- src/internal/terraform_html.go
+++ src/internal/terraform_html.go
@@ -1,8 +1,26 @@
 package internal
 
+import (
+	"bytes"
+	_ "embed"
+	"html/template"
+)
+
+//go:embed templates/report.tpl.html
+var reportTemplate string
+
 type ShowOutputSet []ShowOutput
 
 func (sos *ShowOutputSet) GenerateHTMLReport() []byte {
-	// TODO: Implement this func
-	return []byte("")
+	t, err := template.New("report").Parse(reportTemplate)
+	if err != nil {
+		return nil
+	}
+	var rendered bytes.Buffer
+	err = t.Execute(&rendered, sos)
+	if err != nil {
+		return nil
+	}
+
+	return rendered.Bytes()
 }
--- src/internal/terraform_html_test.go
+++ src/internal/terraform_html_test.go
@@ -1,7 +1,20 @@
 package internal
 
-import "testing"
+import (
+	"encoding/json"
+	"fmt"
+	tfjson "github.com/hashicorp/terraform-json"
+	"github.com/stretchr/testify/assert"
+	"testing"
+)
 
 func TestShowOutputSet_GenerateHTMLReport(t *testing.T) {
-
+	testJson := `{"format_version":"0.1","terraform_version":"0.13.0","planned_values":{"root_module":{"resources":[{"address":"null_resource.this","mode":"managed","type":"null_resource","name":"this","provider_name":"registry.terraform.io/hashicorp/null","schema_version":0},{"address":"random_string.random","mode":"managed","type":"random_string","name":"random","provider_name":"registry.terraform.io/hashicorp/random","schema_version":1,"values":{"keepers":null,"length":16,"lower":true,"min_lower":0,"min_numeric":0,"min_special":0,"min_upper":0,"number":true,"override_special":"/@£$","special":true,"upper":true}}]}},"resource_changes":[{"address":"null_resource.this","mode":"managed","type":"null_resource","name":"this","provider_name":"registry.terraform.io/hashicorp/null","change":{"actions":["create"],"before":null,"after":{},"after_unknown":{"id":true,"triggers":true}}},{"address":"random_string.random","mode":"managed","type":"random_string","name":"random","provider_name":"registry.terraform.io/hashicorp/random","change":{"actions":["create"],"before":null,"after":{"keepers":null,"length":16,"lower":true,"min_lower":0,"min_numeric":0,"min_special":0,"min_upper":0,"number":true,"override_special":"/@£$","special":true,"upper":true},"after_unknown":{"id":true,"result":true}}}],"configuration":{"provider_config":{"null":{"name":"null"},"random":{"name":"random"}},"root_module":{"resources":[{"address":"null_resource.this","mode":"managed","type":"null_resource","name":"this","provider_config_key":"null","expressions":{"triggers":{"references":["random_string.random"]}},"schema_version":0},{"address":"random_string.random","mode":"managed","type":"random_string","name":"random","provider_config_key":"random","expressions":{"length":{"constant_value":16},"override_special":{"constant_value":"/@£$"},"special":{"constant_value":true}},"schema_version":1}]}}}`
+	output := tfjson.Plan{}
+	json.Unmarshal([]byte(testJson), &output)
+	set := ShowOutputSet{}
+	set = append(set, ShowOutput{Plan: output})
+	html := set.GenerateHTMLReport()
+	fmt.Print(html)
+	assert.True(t, len(html) > 0)
 }
